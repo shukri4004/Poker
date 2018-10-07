eb404282a6e03ca9877002fde2fed2d0c8b56e05
shukri4004 <43519352+shukri4004@users.noreply.github.com>
2018-10-07 10:03:11 -0700

    Refactored Hand class
    -Refactored Hand class by updating typeOfHand and implementing other
    methods for the typeOfHand method to use
    -Updated testTypeOfHand method
    -Test succeeded

--- src/main/java/Comp4004/Poker/Hand.java
+++ src/main/java/Comp4004/Poker/Hand.java
@@ -1,6 +1,7 @@
 package Comp4004.Poker;
 
 import java.util.Random; //To get a random card from the deck
+import java.util.Arrays;
 
 public class Hand {
 
@@ -35,11 +36,266 @@ public class Hand {
 	
 	
 	public int typeOfHand() { //according to strategy
-		return 1;
+		if(isRoyalFlush())
+			return 1; //Royal Flush
+		else if(isStraightFlush())
+			return 2; //Straight Flush
+		else if(isFourOfKind())
+			return 3; //Four of a kind
+		else if(isFullHouse())
+			return 4; //Full House
+		else if(isFlush())
+			return 5; //Flush
+		else if(isStraight())
+			return 6; //Straight
+		else if(isThreeOfKind())
+			return 7; //Three of a kind
+		else if(isTwoPair())
+			return 8; //Two pair
+		else if(isPair())
+			return 9; //One pair
+		else
+			return 10; //High Card
 	}
 	
 	
+	public boolean isRoyalFlush() {
+		
+		if(!isFlush())
+			return false;
+		
+		int t = 0;
+		int j = 0;
+		int q = 0;
+		int k = 0;
+		int a = 0;
+		
+		for(int x = 0; x<5; x++) {
+			if(this.cards[x].getRank()=="10")
+				t++;
+			if(this.cards[x].getRank()=="J")
+				j++;
+			if(this.cards[x].getRank()=="Q")
+				q++;
+			if(this.cards[x].getRank()=="K")
+				k++;
+			if(this.cards[x].getRank()=="Ace")
+				a++;
+		}
+		if(t==1 && j==1 && q==1 && k==1 && a==1)//check if one of each card
+			return true;
+		else
+			return false;
+	}
 	
+	public boolean isStraightFlush() {
+		
+		if(!isFlush())
+			return false;
+		
+		int a[]= new int[5];
+		for(int x = 0; x<5; x++) {
+			if(this.cards[x].getRank()=="2")
+				a[x]=1;
+			if(this.cards[x].getRank()=="3")
+				a[x]=2;
+			if(this.cards[x].getRank()=="4")
+				a[x]=3;
+			if(this.cards[x].getRank()=="5")
+				a[x]=4;
+			if(this.cards[x].getRank()=="6")
+				a[x]=5;
+			if(this.cards[x].getRank()=="7")
+				a[x]=6;
+			if(this.cards[x].getRank()=="8")
+				a[x]=7;
+			if(this.cards[x].getRank()=="9")
+				a[x]=8;
+			if(this.cards[x].getRank()=="10")
+				a[x]=9;
+			if(this.cards[x].getRank()=="J")
+				a[x]=10;
+			if(this.cards[x].getRank()=="Q")
+				a[x]=11;
+			if(this.cards[x].getRank()=="K")
+				a[x]=12;
+			if(this.cards[x].getRank()=="Ace")
+				a[x]=13;
+	}
+		 Arrays.sort(a); // Sorting smallest to largest to find a sequence, if any
+		 int y = 0;
+		 for(int x = 0; x<4; x++) {
+				if(a[x]+1==a[x+1])
+					y++;
+		 }
+		 if(y==4)//This means there are five cards in sequential order
+				return true;
+			else
+				return false;
+		
+	}
 	
+	public boolean isFourOfKind() {
+		
+		int y = 0;
+		int z = 0;
+		for(int x = 1; x<5; x++) {
+			if(this.cards[0].getRank()==this.cards[x].getRank())
+				y++;
+		}
+		for(int x = 2; x<5; x++) {
+			if(this.cards[1].getRank()==this.cards[x].getRank())
+				z++;
+		}
+		if(y==3||z==3)//This means there are four cards of the same rank
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean isFullHouse() {
+		if(isThreeOfKind() && isPair())
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean isFlush() {
+		
+		int s = 0;
+		int h = 0;
+		int d = 0;
+		int c = 0;
+		
+		for(int x = 0; x<5; x++) {
+			if(this.cards[x].getSuit()=="S")
+				s++;
+			if(this.cards[x].getSuit()=="H")
+				h++;
+			if(this.cards[x].getSuit()=="D")
+				d++;
+			if(this.cards[x].getSuit()=="C")
+				c++;
+		}
+		if(s==5||h==5||d==5||c==5)//check if hand is a Flush
+			return true;
+		else
+			return false;
+	}
+
+	public boolean isStraight() {
+		int a[]= new int[5];
+		for(int x = 0; x<5; x++) {
+			if(this.cards[x].getRank()=="2")
+				a[x]=1;
+			if(this.cards[x].getRank()=="3")
+				a[x]=2;
+			if(this.cards[x].getRank()=="4")
+				a[x]=3;
+			if(this.cards[x].getRank()=="5")
+				a[x]=4;
+			if(this.cards[x].getRank()=="6")
+				a[x]=5;
+			if(this.cards[x].getRank()=="7")
+				a[x]=6;
+			if(this.cards[x].getRank()=="8")
+				a[x]=7;
+			if(this.cards[x].getRank()=="9")
+				a[x]=8;
+			if(this.cards[x].getRank()=="10")
+				a[x]=9;
+			if(this.cards[x].getRank()=="J")
+				a[x]=10;
+			if(this.cards[x].getRank()=="Q")
+				a[x]=11;
+			if(this.cards[x].getRank()=="K")
+				a[x]=12;
+			if(this.cards[x].getRank()=="Ace")
+				a[x]=13;
+	}
+		 Arrays.sort(a); // Sorting smallest to largest to find a sequence, if any
+		 int y = 0;
+		 for(int x = 0; x<4; x++) {
+				if(a[x]+1==a[x+1])
+					y++;
+		 }
+		 if(y==4)//This means there are five cards in sequential order
+				return true;
+			else
+				return false;
+	}
+	
+	public boolean isThreeOfKind() {
+		int y = 0;
+		int z = 0;
+		int a = 0;
+		for(int x = 1; x<5; x++) {
+			if(this.cards[0].getRank()==this.cards[x].getRank())
+				y++;
+		}
+		for(int x = 2; x<5; x++) {
+			if(this.cards[1].getRank()==this.cards[x].getRank())
+				z++;
+		}
+		for(int x = 3; x<5; x++) {
+			if(this.cards[2].getRank()==this.cards[x].getRank())
+				a++;
+		}
+		if(y>=2||z>=2||a==2)//This means there are three cards of the same rank
+			return true;
+		else
+			return false;
+	}
+	
+	public boolean isTwoPair() {
+		
+		int[] a = new int[5];
+		int s = 0;
+		for(int x = 0; x<5; x++) {
+			s=0;
+			for(int y = 0; y<5; y++) {	
+				if(this.cards[x].getRank()==this.cards[y].getRank() && x!=y)
+					{
+						a[x]=a[x]+1;
+						//s = s + a[x];
+					}
+		}
+		}
+		for(int x = 0; x<5; x++)
+			if (a[x]== 1) 
+					s++; 
+		if(s==4)
+			return true;
+		else return false;
+		
+	}
+	
+	public boolean isPair() { //This method will not be applied if there are more than two of the same kind, because of the threeOfKind method
+
+		int y = 0;
+		int z = 0;
+		int a = 0;
+		int b = 0;
+		for(int x = 0; x<5; x++) {
+			if(this.cards[0].getRank()==this.cards[x].getRank() && x!=0)
+				y++;
+		}
+		for(int x = 0; x<5; x++) {
+			if(this.cards[1].getRank()==this.cards[x].getRank() && x!=1)
+				z++;
+		}
+		for(int x = 0; x<5; x++) {
+			if(this.cards[2].getRank()==this.cards[x].getRank() && x!=2)
+				a++;
+		}
+		for(int x = 0; x<5; x++) {
+			if(this.cards[3].getRank()==this.cards[x].getRank() && x!=3)
+				b++;
+		}
+		if(y==1||z==1||a==1||b==1)//This means there is a pair
+			return true;
+		else
+			return false;
+	}
 	
 }
--- src/test/java/Comp4004/Poker/TestHand.java
+++ src/test/java/Comp4004/Poker/TestHand.java
@@ -6,26 +6,65 @@ public class TestHand extends TestCase {
 
 	public void testTypeOfHand() {//There are ten types of hands: 1=Royal Flush, 2=Straight Flush, ... 
 		Hand[] h = new Hand[10];
+		//Royal Flush
 		h[0] = new Hand();
+		for(int i=0; i<5; i++)
+			h[0].cards[i].suit="H";
+		h[0].cards[0].rank="10"; h[0].cards[1].rank="J"; h[0].cards[2].rank="Q"; h[0].cards[3].rank="K"; h[0].cards[4].rank="Ace";
 		assertEquals(1, h[0].typeOfHand());
+		
+		//Straight Flush
 		h[1] = new Hand();
-		assertEquals(1, h[1].typeOfHand());
+		for(int i=0; i<5; i++)
+			h[1].cards[i].suit="S";
+		h[1].cards[0].rank="5"; h[1].cards[1].rank="6"; h[1].cards[2].rank="7"; h[1].cards[3].rank="8"; h[1].cards[4].rank="9";
+		assertEquals(2, h[1].typeOfHand());
+		
+	    //Four of a Kind
 		h[2] = new Hand();
-		assertEquals(1, h[2].typeOfHand());
+		for(int i=0; i<4; i++)
+			h[2].cards[i].rank="J";
+		h[2].cards[4].rank="3";
+		assertEquals(3, h[2].typeOfHand());
+		
+		//Full House
 		h[3] = new Hand();
-		assertEquals(1, h[3].typeOfHand());
+		h[3].cards[0].rank="5"; h[3].cards[1].rank="5"; h[3].cards[2].rank="5"; h[3].cards[3].rank="9"; h[3].cards[4].rank="9";
+		assertEquals(4, h[3].typeOfHand());
+		
+		//Flush
 		h[4] = new Hand();
-		assertEquals(1, h[4].typeOfHand());
+		for(int i=0; i<5; i++)
+			h[4].cards[i].suit="H";
+		h[4].cards[0].rank="2"; h[4].cards[1].rank="6"; h[4].cards[2].rank="7"; h[4].cards[3].rank="J"; h[4].cards[4].rank="K";
+		assertEquals(5, h[4].typeOfHand());
+		
+		//Straight
 		h[5] = new Hand();
-		assertEquals(1, h[5].typeOfHand());
+		h[5].cards[0].rank="5"; h[5].cards[1].rank="6"; h[5].cards[2].rank="7"; h[5].cards[3].rank="8"; h[5].cards[4].rank="9";
+		h[5].cards[0].suit="H"; h[5].cards[1].suit="D"; h[5].cards[2].suit="S";
+		assertEquals(6, h[5].typeOfHand());
+		
+		//Three of a Kind
 		h[6] = new Hand();
-		assertEquals(1, h[6].typeOfHand());
+		h[6].cards[0].rank="5"; h[6].cards[1].rank="5"; h[6].cards[2].rank="5"; h[6].cards[3].rank="2"; h[6].cards[4].rank="9";
+		assertEquals(7, h[6].typeOfHand());
+		
+		//Two pair
 		h[7] = new Hand();
-		assertEquals(1, h[7].typeOfHand());
+		h[7].cards[0].rank="5"; h[7].cards[1].rank="5"; h[7].cards[2].rank="10"; h[7].cards[3].rank="10"; h[7].cards[4].rank="9";
+		h[7].cards[0].suit="S"; h[7].cards[1].suit="H"; h[7].cards[2].suit="D"; h[7].cards[3].suit="C"; 
+		assertEquals(8, h[7].typeOfHand());
+		
+		//One Pair
 		h[8] = new Hand();
-		assertEquals(1, h[8].typeOfHand());
+		h[8].cards[0].rank="5"; h[8].cards[1].rank="5"; h[8].cards[2].rank="10"; h[8].cards[3].rank="3"; h[8].cards[4].rank="9";
+		assertEquals(9, h[8].typeOfHand());
+		
+		//High Card
 		h[9] = new Hand();
-		assertEquals(1, h[9].typeOfHand());
+		h[9].cards[0].rank="5"; h[9].cards[1].rank="Q"; h[9].cards[2].rank="2"; h[9].cards[3].rank="10"; h[9].cards[4].rank="9";
+		assertEquals(10, h[9].typeOfHand());
 
 	}
 }
