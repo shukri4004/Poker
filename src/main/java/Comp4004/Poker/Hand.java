a38047f627ab24b7c9ed01497c09dd22513c9491
shukri4004 <43519352+shukri4004@users.noreply.github.com>
2018-10-08 20:21:07 -0700

    Finished applyStrategy
    -refactored applyStrategy method to handle remaining strategies
    -test succeeded

--- src/main/java/Comp4004/Poker/Hand.java
+++ src/main/java/Comp4004/Poker/Hand.java
@@ -467,6 +467,190 @@ public class Hand {
 			}
 		}
 	}
+	else if(isThreeOfSameSuit()) {
+		for(int x = 1; x<5; x++) {
+			if(this.cards[0].getSuit()==this.cards[x].getSuit())
+				a=1;
+		}
+		for(int x = 0; x<5; x++) {
+			if(this.cards[1].getSuit()==this.cards[x].getSuit() && x!=1)
+				b=1;
+		}
+		for(int x = 0; x<5; x++) {
+			if(this.cards[2].getSuit()==this.cards[x].getSuit() && x!=2)
+				c=1;
+		}
+		for(int x = 0; x<5; x++) {
+			if(this.cards[3].getSuit()==this.cards[x].getSuit() && x!=3)
+				y=1;
+		}
+		for(int x = 0; x<4; x++) {
+			if(this.cards[4].getSuit()==this.cards[x].getSuit())
+				z=1;
+		}
+		if(a==0){
+			rd = rand.nextInt(52);
+			this.cards[0] = d.getCard(rd);
+			this.values[0] = rd;
+		}
+		if(b==0){
+			rd = rand.nextInt(52);
+			this.cards[1] = d.getCard(rd);
+			this.values[1] = rd;
+		}
+		if(c==0){
+			rd = rand.nextInt(52);
+			this.cards[2] = d.getCard(rd);
+			this.values[2] = rd;
+		}
+		if(y==0){
+			rd = rand.nextInt(52);
+			this.cards[3] = d.getCard(rd);
+			this.values[3] = rd;
+		}
+		if(z==0){
+			rd = rand.nextInt(52);
+			this.cards[4] = d.getCard(rd);
+			this.values[4] = rd;
+		}
+
+	}
+	
+	else if(isThreeCardSequence()) { //Exchange the two cards not in sequential order
+									//After sorting assigned numbers to the cards, either the first 2, the last 2, or the first and last are non-sequential
+									//Assigned numbers to the cards is done by array variable below
+		int arr[]= new int[5];
+		int arr2[]= new int[5];
+		int n;
+		for(int x = 0; x<5; x++) {
+			if(this.cards[x].getRank()=="2")
+				arr[x]=1;
+			if(this.cards[x].getRank()=="3")
+				arr[x]=2;
+			if(this.cards[x].getRank()=="4")
+				arr[x]=3;
+			if(this.cards[x].getRank()=="5")
+				arr[x]=4;
+			if(this.cards[x].getRank()=="6")
+				arr[x]=5;
+			if(this.cards[x].getRank()=="7")
+				arr[x]=6;
+			if(this.cards[x].getRank()=="8")
+				arr[x]=7;
+			if(this.cards[x].getRank()=="9")
+				arr[x]=8;
+			if(this.cards[x].getRank()=="10")
+				arr[x]=9;
+			if(this.cards[x].getRank()=="J")
+				arr[x]=10;
+			if(this.cards[x].getRank()=="Q")
+				arr[x]=11;
+			if(this.cards[x].getRank()=="K")
+				arr[x]=12;
+			if(this.cards[x].getRank()=="Ace")
+				arr[x]=13;
+	} 
+		
+		for(int x = 0; x<5; x++) {
+			arr2[x]=arr[x];
+		}
+		Arrays.sort(arr); // Sorting smallest to largest to find a sequence, if any
+		if(arr[1]+1!=arr[2]) {
+			i=0;
+			n=1;
+		}
+		else if(arr[2]+1!=arr[3]) {
+			i=3;
+			n=4;
+		}
+		else {
+			i=0;
+			n=4;
+		}
+		for(int x = 0; x<5; x++) {
+			if(arr2[x]==arr[i]||arr2[x]==arr[n]) {
+				rd = rand.nextInt(52);
+				this.cards[x] = d.getCard(rd);
+				this.values[x] = rd;
+			}
+		}
+	}
+			
+	else if(this.typeOfHand()==9) { //One pair
+		for(int x = 1; x<5; x++) {
+			if(this.cards[0].getRank()==this.cards[x].getRank())
+				a=1;
+		}
+		for(int x = 0; x<5; x++) {
+			if(this.cards[1].getRank()==this.cards[x].getRank() && x!=1)
+				b=1;
+		}
+		for(int x = 0; x<5; x++) {
+			if(this.cards[2].getRank()==this.cards[x].getRank() && x!=2)
+				c=1;
+		}
+		for(int x = 0; x<5; x++) {
+			if(this.cards[3].getRank()==this.cards[x].getRank() && x!=3)
+				y=1;
+		}
+		for(int x = 0; x<4; x++) {
+			if(this.cards[4].getRank()==this.cards[x].getRank())
+				z=1;
+		}
+		if(a==0){
+			rd = rand.nextInt(52);
+			this.cards[0] = d.getCard(rd);
+			this.values[0] = rd;
+		}
+		if(b==0){
+			rd = rand.nextInt(52);
+			this.cards[1] = d.getCard(rd);
+			this.values[1] = rd;
+		}
+		if(c==0){
+			rd = rand.nextInt(52);
+			this.cards[2] = d.getCard(rd);
+			this.values[2] = rd;
+		}
+		if(y==0){
+			rd = rand.nextInt(52);
+			this.cards[3] = d.getCard(rd);
+			this.values[3] = rd;
+		}
+		if(z==0){
+			rd = rand.nextInt(52);
+			this.cards[4] = d.getCard(rd);
+			this.values[4] = rd;
+		}
+}
+	else //High card
+	{
+		// Get the last 3 cards from hand.values, it's a sorted array in ascendent order
+		int i2, i3;
+		Arrays.sort(values);
+		i = this.values[0];
+		i2 = this.values[1];
+		i3 = this.values[2];
+		
+		for(int x=0; x<5; x++) {
+			if(this.cards[x].suit==d.cardHolder[i].suit && this.cards[x].rank==d.cardHolder[i].rank) {
+				rd = rand.nextInt(52);
+				this.cards[x] = d.getCard(rd);
+				this.values[x] = rd;
+			}
+			if(this.cards[x].suit==d.cardHolder[i2].suit && this.cards[x].rank==d.cardHolder[i2].rank) {
+				rd = rand.nextInt(52);
+				this.cards[x] = d.getCard(rd);
+				this.values[x] = rd;
+			}
+			if(this.cards[x].suit==d.cardHolder[i3].suit && this.cards[x].rank==d.cardHolder[i3].rank) {
+				rd = rand.nextInt(52);
+				this.cards[x] = d.getCard(rd);
+				this.values[x] = rd;
+			}
+		}
+	}
+		
 	}
 	
 	public boolean oneCardAwayFromFullHouse() {
@@ -546,6 +730,70 @@ public boolean oneCardAwayFromStraight() {//check if one away from straight
 		else
 			return false;
 	
+}
+
+public boolean isThreeOfSameSuit() {
+	int y = 0;
+	int z = 0;
+	int a = 0;
+	for(int x = 1; x<5; x++) {
+		if(this.cards[0].getSuit()==this.cards[x].getSuit())
+			y++;
+	}
+	for(int x = 2; x<5; x++) {
+		if(this.cards[1].getSuit()==this.cards[x].getSuit())
+			z++;
+	}
+	for(int x = 3; x<5; x++) {
+		if(this.cards[2].getSuit()==this.cards[x].getSuit())
+			a++;
+	}
+	if(y>=2||z>=2||a==2)//This means there are three cards of the same suit
+		return true;
+	else
+		return false;
+}
+
+public boolean isThreeCardSequence() {
+	int a[]= new int[5];
+	for(int x = 0; x<5; x++) {
+		if(this.cards[x].getRank()=="2")
+			a[x]=1;
+		if(this.cards[x].getRank()=="3")
+			a[x]=2;
+		if(this.cards[x].getRank()=="4")
+			a[x]=3;
+		if(this.cards[x].getRank()=="5")
+			a[x]=4;
+		if(this.cards[x].getRank()=="6")
+			a[x]=5;
+		if(this.cards[x].getRank()=="7")
+			a[x]=6;
+		if(this.cards[x].getRank()=="8")
+			a[x]=7;
+		if(this.cards[x].getRank()=="9")
+			a[x]=8;
+		if(this.cards[x].getRank()=="10")
+			a[x]=9;
+		if(this.cards[x].getRank()=="J")
+			a[x]=10;
+		if(this.cards[x].getRank()=="Q")
+			a[x]=11;
+		if(this.cards[x].getRank()=="K")
+			a[x]=12;
+		if(this.cards[x].getRank()=="Ace")
+			a[x]=13;
+}
+	 Arrays.sort(a); // Sorting smallest to largest to find a sequence, if any
+	 int y = 0;
+	 for(int x = 0; x<4; x++) {
+			if(a[x]+1==a[x+1])
+				y++;
+	 }
+	 if(y==2)//This means there are three cards in sequential order
+			return true;
+		else
+			return false;
 }
 
 
